71d9a3937269c7bd3872ada56a1b7aed90aea851
Austin Elliott <20austine12@gmail.com>
2023-08-22 00:29:56 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MapComponent from './TimeZoneMap';  // Import the component
+
+function App() {
+    
+    return (
+        <div className="App">
+            <MapComponent />  {/* Use the component */}
+        </div>
+    );
+}
+
+export default App;
